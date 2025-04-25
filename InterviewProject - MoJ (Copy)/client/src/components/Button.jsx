d443effe123dcb4443cfd0ba9fc07c41b8e1d36e
BenPaterson97 <132564660+BenPaterson97@users.noreply.github.com>
2025-04-25 13:37:26 +0100

    Delete InterviewProject - MoJ (Copy) directory

--- InterviewProject - MoJ (Copy)/client/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ label, onClick }) => {
-  return (
-    <button className="btn btn-primary" aria-label={label} onClick={onClick}>
-      {label}
-    </button>
-  );
-};
-
-export default Button;
